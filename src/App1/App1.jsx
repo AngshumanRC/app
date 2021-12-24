2127773fa4a55a5c1f628bb16f2e9bbb967c595e
Angshu1997 <arc290897@gmail.com>
2021-12-24 22:21:10 +0530

    Adding

---
+++ src/App1/App1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App1.css';
+
+const App1 = () => {
+    return(
+        <div className='container'>
+            <div className='header'></div>
+            <div className='content'>
+                <div className='navbar'>
+                    <div className='nav1'></div>
+                    <div className='nav2'></div>
+                    <div className='nav3  '></div>
+                </div>
+                <div className='main'>
+                <div className='main1'></div>
+                <div className='main2'></div>
+                <div className='main3'>
+                    <div className='one'>
+                        <div className='one-one'></div>
+                        <div className='one-two'></div>
+                    </div>
+                    <div className='two'></div>
+                    <div className='three'></div>
+                    <div className='four'></div>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App1;
